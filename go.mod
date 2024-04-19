2a6e527a881b51f16a0d7c5d60347064de6af0a0
Josh Glendenning <git@isobit.io>
2024-04-19 00:28:48 -0500

    WIP

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/gorilla/websocket v1.5.0
 	github.com/isobit/cli v0.11.1-0.20240207065020-f91febcb7277
 	github.com/jackc/pgx/v5 v5.4.3
+	github.com/miekg/dns v1.1.59
 	github.com/sourcegraph/conc v0.3.0
 	github.com/stretchr/testify v1.8.1
 	github.com/tinylib/msgp v1.1.8
@@ -22,8 +23,11 @@ require (
 	github.com/kr/text v0.2.0 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.12.0 // indirect
-	golang.org/x/sys v0.11.0 // indirect
-	golang.org/x/text v0.12.0 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+	golang.org/x/mod v0.16.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/schemes/dns/dns.go
+++ internal/schemes/dns/dns.go
@@ -6,33 +6,106 @@ import (
 	"fmt"
 	// "io"
 	"net"
+	"reflect"
 	"strings"
 
 	"github.com/isobit/ndog/internal"
-	// "github.com/isobit/ndog/internal/log"
+	"github.com/isobit/ndog/internal/log"
+
+	"github.com/miekg/dns"
 )
 
 var Scheme = &ndog.Scheme{
-	Names:   []string{"dns"},
-	Connect: Connect,
+	Names:             []string{"dns"},
+	Connect:           Connect,
+	ConnectOptionHelp: connectOptionHelp,
 }
 
+var connectOptionHelp = ndog.OptionsHelp{}.
+	Add("norecurse", "", "disable recursion")
+
+type connectOptions struct {
+	NoRecurse bool
+}
+
+func extractConnectOptions(opts ndog.Options) (connectOptions, error) {
+	o := connectOptions{}
+
+	if _, ok := opts.Pop("norecurse"); ok {
+		o.NoRecurse = true
+	}
+
+	return o, opts.Done()
+}
 func Connect(cfg ndog.ConnectConfig) error {
 	ctx := context.TODO()
-	r := net.Resolver{}
 
-	if cfg.URL.Host != "" {
-		return fmt.Errorf("specifying a nameserver is not supported yet")
+	opts, err := extractConnectOptions(cfg.Options)
+	if err != nil {
+		return err
 	}
+
+	// if cfg.URL.Host != "" {
+	// 	return fmt.Errorf("specifying a nameserver is not supported yet")
+	// }
 
 	path := strings.SplitN(strings.TrimPrefix(cfg.URL.Path, "/"), "/", 2)
 
 	host := path[0]
 
-	var typ string
+	typ := "A"
 	if len(path) == 2 {
 		typ = path[1]
 	}
+
+	nameserver := cfg.URL.Host
+	if nameserver != "" {
+		qtype, ok := dns.StringToType[strings.ToUpper(typ)]
+		if !ok {
+			return fmt.Errorf("unknown type: %s", typ)
+		}
+
+		client := &dns.Client{}
+		req := &dns.Msg{
+			MsgHdr: dns.MsgHdr{
+				Id:               dns.Id(),
+				Opcode:           dns.OpcodeQuery,
+				RecursionDesired: !opts.NoRecurse,
+			},
+			Question: []dns.Question{{
+				Name:   dns.Fqdn(host),
+				Qtype:  qtype,
+				Qclass: dns.ClassINET,
+			}},
+		}
+		log.Logf(10, "query:")
+		log.Logf(10, req.String())
+		res, _, err := client.Exchange(req, nameserver)
+		if err != nil {
+			return err
+		}
+		log.Logf(10, "answer:")
+		log.Logf(10, res.String())
+		for _, answer := range res.Answer {
+			switch ans := answer.(type) {
+			case *dns.A:
+				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.A)
+			case *dns.AAAA:
+				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.AAAA)
+			case *dns.CNAME:
+				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.Target)
+			default:
+				return fmt.Errorf("unexpected answer type: %s", reflect.TypeOf(ans))
+			}
+			// r, ok := ans.(*dns.A)
+			// if !ok {
+			// }
+			// fmt.Fprintf(cfg.Stream.Writer, "%s\n", r.A)
+		}
+		return nil
+	}
+
+	r := net.Resolver{}
 
 	switch typ {
 	case "", "A", "AAAA":
