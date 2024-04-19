ee1ca828b0b4d33e1a8a770649320ff6e42252a8
Josh Glendenning <git@isobit.io>
2024-04-19 00:43:38 -0500

    WIP

--- internal/schemes/dns/dns.go
+++ internal/schemes/dns/dns.go
@@ -1,11 +1,9 @@
 package dns
 
 import (
-	"context"
-	// "errors"
+	"encoding/json"
 	"fmt"
-	// "io"
-	"net"
+	"io"
 	"reflect"
 	"strings"
 
@@ -22,10 +20,12 @@ var Scheme = &ndog.Scheme{
 }
 
 var connectOptionHelp = ndog.OptionsHelp{}.
-	Add("norecurse", "", "disable recursion")
+	Add("norecurse", "", "disable recursion").
+	Add("json", "", "use JSON representation for answers")
 
 type connectOptions struct {
 	NoRecurse bool
+	JSON      bool
 }
 
 func extractConnectOptions(opts ndog.Options) (connectOptions, error) {
@@ -35,22 +35,20 @@ func extractConnectOptions(opts ndog.Options) (connectOptions, error) {
 		o.NoRecurse = true
 	}
 
+	if _, ok := opts.Pop("json"); ok {
+		o.JSON = true
+	}
+
 	return o, opts.Done()
 }
 func Connect(cfg ndog.ConnectConfig) error {
-	ctx := context.TODO()
-
 	opts, err := extractConnectOptions(cfg.Options)
 	if err != nil {
 		return err
 	}
 
-	// if cfg.URL.Host != "" {
-	// 	return fmt.Errorf("specifying a nameserver is not supported yet")
-	// }
-
+	nameserver := cfg.URL.Host
 	path := strings.SplitN(strings.TrimPrefix(cfg.URL.Path, "/"), "/", 2)
-
 	host := path[0]
 
 	typ := "A"
@@ -58,106 +56,65 @@ func Connect(cfg ndog.ConnectConfig) error {
 		typ = path[1]
 	}
 
-	nameserver := cfg.URL.Host
-	if nameserver != "" {
-		qtype, ok := dns.StringToType[strings.ToUpper(typ)]
-		if !ok {
-			return fmt.Errorf("unknown type: %s", typ)
-		}
+	qtype, ok := dns.StringToType[strings.ToUpper(typ)]
+	if !ok {
+		return fmt.Errorf("unknown type: %s", typ)
+	}
 
-		client := &dns.Client{}
-		req := &dns.Msg{
-			MsgHdr: dns.MsgHdr{
-				Id:               dns.Id(),
-				Opcode:           dns.OpcodeQuery,
-				RecursionDesired: !opts.NoRecurse,
-			},
-			Question: []dns.Question{{
-				Name:   dns.Fqdn(host),
-				Qtype:  qtype,
-				Qclass: dns.ClassINET,
-			}},
-		}
-		log.Logf(10, "query:")
-		log.Logf(10, req.String())
-		res, _, err := client.Exchange(req, nameserver)
+	client := &dns.Client{}
+	req := &dns.Msg{
+		MsgHdr: dns.MsgHdr{
+			Id:               dns.Id(),
+			Opcode:           dns.OpcodeQuery,
+			RecursionDesired: !opts.NoRecurse,
+		},
+		Question: []dns.Question{{
+			Name:   dns.Fqdn(host),
+			Qtype:  qtype,
+			Qclass: dns.ClassINET,
+		}},
+	}
+
+	log.Logf(10, "query:")
+	log.Logf(10, req.String())
+
+	res, _, err := client.Exchange(req, nameserver)
+	if err != nil {
+		return err
+	}
+
+	log.Logf(10, "answer:")
+	log.Logf(10, res.String())
+
+	if opts.JSON {
+		data, err := json.Marshal(res)
 		if err != nil {
 			return err
 		}
-		log.Logf(10, "answer:")
-		log.Logf(10, res.String())
-		for _, answer := range res.Answer {
-			switch ans := answer.(type) {
-			case *dns.A:
-				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.A)
-			case *dns.AAAA:
-				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.AAAA)
-			case *dns.CNAME:
-				fmt.Fprintf(cfg.Stream.Writer, "%s\n", ans.Target)
-			default:
-				return fmt.Errorf("unexpected answer type: %s", reflect.TypeOf(ans))
-			}
-			// r, ok := ans.(*dns.A)
-			// if !ok {
-			// }
-			// fmt.Fprintf(cfg.Stream.Writer, "%s\n", r.A)
-		}
+		cfg.Stream.Writer.Write(data)
+		io.WriteString(cfg.Stream.Writer, "\n")
 		return nil
 	}
 
-	r := net.Resolver{}
-
-	switch typ {
-	case "", "A", "AAAA":
-		var network string
-		switch typ {
-		case "":
-			network = "ip"
-		case "A":
-			network = "ip4"
-		case "AAAA":
-			network = "ip6"
+	for _, answer := range res.Answer {
+		switch ans := answer.(type) {
+		case *dns.A:
+			fmt.Fprintln(cfg.Stream.Writer, ans.A)
+		case *dns.AAAA:
+			fmt.Fprintln(cfg.Stream.Writer, ans.AAAA)
+		case *dns.CNAME:
+			fmt.Fprintln(cfg.Stream.Writer, ans.Target)
+		case *dns.TXT:
+			for _, txt := range ans.Txt {
+				fmt.Fprintln(cfg.Stream.Writer, txt)
+			}
+		case *dns.MX:
+			fmt.Fprintln(cfg.Stream.Writer, ans.Mx)
+		case *dns.NS:
+			fmt.Fprintln(cfg.Stream.Writer, ans.Ns)
+		default:
+			return fmt.Errorf("unexpected answer type: %s", reflect.TypeOf(ans))
 		}
-		recs, err := r.LookupIP(ctx, network, host)
-		if err != nil {
-			return err
-		}
-		for _, rec := range recs {
-			fmt.Fprintln(cfg.Stream.Writer, rec.String())
-		}
-	case "CNAME":
-		rec, err := r.LookupCNAME(ctx, host)
-		if err != nil {
-			return err
-		}
-		fmt.Fprintln(cfg.Stream.Writer, rec)
-	case "TXT":
-		recs, err := r.LookupTXT(ctx, host)
-		if err != nil {
-			return err
-		}
-		for _, rec := range recs {
-			fmt.Fprintln(cfg.Stream.Writer, rec)
-		}
-	case "NS":
-		recs, err := r.LookupNS(ctx, host)
-		if err != nil {
-			return err
-		}
-		for _, rec := range recs {
-			fmt.Fprintln(cfg.Stream.Writer, rec.Host)
-		}
-	case "MX":
-		recs, err := r.LookupMX(ctx, host)
-		if err != nil {
-			return err
-		}
-		for _, rec := range recs {
-			fmt.Fprintf(cfg.Stream.Writer, "%s %d\n", rec.Host, rec.Pref)
-		}
-	default:
-		return fmt.Errorf("unsupported record type: %s", typ)
 	}
-
 	return nil
 }
