69980238d600a7cf30ac1e2ad9c2842b032cf268
Josh Glendenning <git@isobit.io>
2024-04-18 22:21:25 -0500

    http: add proxy_pass option

--- internal/schemes/http/listen.go
+++ internal/schemes/http/listen.go
@@ -18,6 +18,7 @@ import (
 type listenOptions struct {
 	StatusCode    int
 	Headers       map[string]string
+	ProxyPass     string
 	ServeFile     string
 	MsgpackToJSON bool
 }
@@ -25,6 +26,7 @@ type listenOptions struct {
 var listenOptionHelp = ndog.OptionsHelp{}.
 	Add("header.<NAME>", "<VALUE>", "extra response headers to send").
 	Add("msgpack_to_json", "", "attempt to convert msgpack-encoded requests to JSON").
+	Add("proxy_pass", "<URL>", "proxy requests to another server").
 	Add("serve_file", "<PATH>", "use Go's ServeFile to serve files relative to this directory").
 	Add("status_code", "<CODE>", "status code to send in response (default: 200)")
 
@@ -43,16 +45,16 @@ func extractListenOptions(opts ndog.Options) (listenOptions, error) {
 		}
 	}
 
+	if proxyPass, ok := opts.Pop("proxy_pass"); ok {
+		o.ProxyPass = proxyPass
+	}
+
 	if serveFilePath, ok := opts.Pop("serve_file"); ok {
 		serveFileAbsPath, err := filepath.Abs(serveFilePath)
 		if err != nil {
 			return o, fmt.Errorf("error parsing serve_file option: %w", err)
 		}
 		o.ServeFile = serveFileAbsPath
-	}
-
-	if proxyPass, ok := opts.Pop("proxy_pass"); ok {
-		o.ProxyPass = proxyPass
 	}
 
 	headerKeyPrefix := "header."
@@ -99,6 +101,10 @@ func Listen(cfg ndog.ListenConfig) error {
 			}
 			if opts.ServeFile != "" {
 				http.ServeFile(w, r, filepath.Join(opts.ServeFile, r.URL.Path))
+				return
+			}
+			if opts.ProxyPass != "" {
+				proxyPass(w, r, opts.ProxyPass)
 				return
 			}
 
@@ -151,4 +157,36 @@ func Listen(cfg ndog.ListenConfig) error {
 	}
 	log.Logf(0, "listening: %s", s.Addr)
 	return s.ListenAndServe()
+}
+
+func proxyPass(w http.ResponseWriter, r *http.Request, passUrl string) {
+	pr, err := http.NewRequest(r.Method, passUrl, r.Body)
+	pr.URL.Path = r.URL.Path
+	pr.URL.Fragment = r.URL.Fragment
+	pr.URL.RawQuery = r.URL.RawQuery
+	for key, vals := range r.Header {
+		for _, val := range vals {
+			pr.Header.Set(key, val)
+		}
+	}
+	forwardedProto := "http"
+	if r.TLS != nil {
+		forwardedProto = "https"
+	}
+	pr.Header.Add("Forwarded", fmt.Sprintf("for=%s;proto=%s;host=%s", r.RemoteAddr, forwardedProto, r.Host))
+
+	resp, err := http.DefaultClient.Do(pr)
+	if err != nil {
+		log.Logf(-1, "failed to proxy request: %s", err)
+		w.WriteHeader(http.StatusBadGateway)
+		return
+	}
+	defer resp.Body.Close()
+	for key, vals := range resp.Header {
+		for _, val := range vals {
+			w.Header().Set(key, val)
+		}
+	}
+	w.WriteHeader(resp.StatusCode)
+	io.Copy(w, resp.Body)
 }
